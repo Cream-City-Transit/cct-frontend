6eab622fb243beabe717748c874371432cac6c60
SMaillis <153215256+smaillis@users.noreply.github.com>
2026-09-17 17:15:03 -0500

    frontend setup and base map working

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.tsx';
+import Navigation from './control_inputs'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+      <Navigation/>
+      <App />
+  </React.StrictMode>
+);
