7b8ec6f65cb1350de5c68d4af6b3a3e4a562b8de
SMaillis <153215256+SMaillis@users.noreply.github.com>
2026-09-24 16:17:43 -0500

    start and endpoint indicators can be drag and dropped

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,12 @@ import BaseMap from "@opentripplanner/base-map";
 import { IntlProvider } from "react-intl";
 import "./app.css";
 import React from "react";
+import EndpointsOverlay from "@opentripplanner/endpoints-overlay";
 import Navigation from "./control_inputs";
 import TrainIcon from "./train_icon";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+
 
 function App() {
   return (
@@ -27,10 +31,15 @@ function App() {
         <main className="baseMap">
           <BaseMap
             center={[43.075191, -87.881391]}
-            zoom={15}
-          />
+            zoom={15}>
+            <EndpointsOverlay
+              fromLocation={{lat: 43.075191, lon: -87.881391, name: "from"}}
+              toLocation={{lat: 43.075191, lon: -87.880391, name: "to"}}
+            /> 
+          </BaseMap>
 
           <Navigation />
+         
         </main>
 
       </div>
