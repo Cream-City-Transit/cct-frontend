afa3225f01002dfbb7f13a3f73ba90919ac88e47
tkknight13 <knight4@uwm.edu>
2026-09-24 23:19:05 -0500

    minimize search bar function implemented

--- src/control_inputs.tsx
+++ src/control_inputs.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { useState } from 'react';
 import Switch from './switch';
+
 function Navigation() {
     const [origin, setOrigin] = useState("");
     const [destination, setDestination] = useState("");
-    return (
-        <form className="navigation">
-            <input type="text" className="search_input" value={origin} placeholder="Starting Location"
-                   onChange={(location) => setOrigin(location.target.value)}/><br/>
-            <input type="text" className="search_input" value={destination} placeholder="Destination"
-                   onChange={(location) => setDestination(location.target.value)}/><br/>
-            <button type="submit">Search</button><br/>
+    const [minimized, setMinimized] = useState(false);
 
-            <Switch mode="MCTS"/>
-            <Switch mode="Prowl Line"/>
-            <Switch mode="U-PARK"/>
-            <Switch mode="The Hop"/>
+    return (
+        <form className={`navigation ${minimized ? "minimized" : ""}`}>
+            <button type="button" className="minimize_button" onClick={() => setMinimized(!minimized)}>
+                {minimized ? "▲" : "▼"}
+            </button>
+            {!minimized && (
+                <>
+                    <input type="text" className="search_input" value={origin} placeholder="Starting Location"
+                    onChange={(location) => setOrigin(location.target.value)}/><br/>
+                    <input type="text" className="search_input" value={destination} placeholder="Destination"
+                    onChange={(location) => setDestination(location.target.value)}/><br/>
+                    <button type="submit">Search</button><br/>
+
+                    <Switch mode="MCTS"/>
+                    <Switch mode="Prowl Line"/>
+                    <Switch mode="U-PARK"/>
+                    <Switch mode="The Hop"/>
+                </>
+            )}
         </form>
-    )
+    );
 }
 
 export default Navigation;
